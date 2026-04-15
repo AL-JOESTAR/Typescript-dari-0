1e7d3422fdd1d4821f8a914388d84a1ff0122d52
Sainzel <cppnaf@gmail.com>
2026-04-15 15:42:30 +0700

    function

---
+++ README.md
@@ -0,0 +1,41 @@
+### Pastikan sebelumnya terinstal :
+- node js
+- npm
+
+### Instal Typescript
+
+```properties
+npm i -g typescript
+```
+
+<h1>Cek Typescript</h1>
+
+```properties
+tsc --v
+```
+
+<h1>init project</h1>
+
+```properties
+tsc --init
+```
+
+## Set Config
+
+<p>Masuk ke tsconfig.json</p>
+
+Aktifkan dan ubah
+
+```properties
+"rootDir": "./src"
+"outDir": "./bundle"
+
+"noImplicitReturns": true,
+"noImplicitOverride": true,
+```
+
+### Run Typescript
+
+```properties
+tsc && node bundle/index.js
+```
--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,46 @@ biasa = ["al", 'Joestar'];
 let tuple: [string, number, boolean];
 tuple = ["al", 5, true];
 
+// array level
+let level: (string | boolean)[];
+level = ["halo" , "nama saya", true]
 
+// var namasaya = "al";
+
+// Compile
 console.log(nama); // tanpa object
 console.log({nomor});
 console.log({IsDead});
 console.log({biasa});
 console.log({tuple});
+// console.log({namasaya});
+console.log({level});
+
+//object
+let mahasiswa: object[] = [
+    {nim:123, nama:"reza"},
+    {nim:456, nama:"anton"},
+]
+
+console.log (mahasiswa); // {} untuk menampilkan object
+console.log (typeof mahasiswa);// untuk melihat tipe data
+
+// function 
+function tambah (a:number,b:number){
+    return a + b;
+}
+
+console.log(tambah(1,5));
+
+function tambah2 (a:number,b:number): void{ // tidak mengeluarkan return
+    let tambah =  a + b;
+    console.log(tambah)
+}
+
+tambah2(1,7);
+
+function pesan (pesan:string): string{
+    return pesan;
+}
+
+console.log(pesan("halo dunia"));
